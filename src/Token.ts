e42eabdbf028935317d318f54339ed74beb8db7e
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-23 15:46:18 +0800

    refactor 0.0.8

--- src/Token.ts
+++ src/Token.ts
@@ -1,12 +1,10 @@
 import { TokenType } from "./TokenType";
 
-// export namespace io.github.yumika {
-  export class Token {
-    constructor(
-      public readonly type: TokenType,
-      public readonly lexeme: string,
-      public readonly literal: Object | null,
-      public line: Number
-    ) {}
-  }
-// }
+export class Token {
+  constructor(
+    public readonly type: TokenType,
+    public readonly lexeme: string,
+    public readonly literal: Object | null,
+    public line: Number
+  ) {}
+}
