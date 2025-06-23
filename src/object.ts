6011f38919c3c236ce308f686442fda9d3b7010a
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-23 15:40:15 +0800

    refactor 0.0.8

--- src/object.ts
+++ src/object.ts
@@ -16,155 +16,155 @@ import { Interpreter } from "./Interpreter";
 
 import { RuntimeError } from "./RuntimeError";
 
-  export interface YmkCallable extends Object {
-    arity(): number;
-    call(interpreter: Interpreter, _arguments: Object[]): Object | undefined | null;
+export interface YmkCallable extends Object {
+  arity(): number;
+  call(interpreter: Interpreter, _arguments: Object[]): Object | undefined | null;
+}
+
+export class YmkClass implements YmkCallable {
+  constructor(
+    public readonly name: string,
+    public superclass: YmkClass | null,
+    public methods: Map<String, YmkFunction>)
+  {}
+
+  findMethod(name: string): YmkFunction | null {
+    if (this.methods.has(name)) {
+      return this.methods.get(name)!;
+    }
+
+    if (this.superclass != null) {
+      return this.superclass.findMethod(name);
+    }
+
+    return null;
   }
 
-  export class YmkClass implements YmkCallable {
-    constructor(
-      public readonly name: string,
-      public superclass: YmkClass | null,
-      public methods: Map<String, YmkFunction>)
-    {}
+  toString(): string { return this.name; }
 
-    findMethod(name: string): YmkFunction | null {
-      if (this.methods.has(name)) {
-        return this.methods.get(name)!;
+  public call(interpreter: Interpreter,
+    _arguments: Object[]
+  ) {
+    const instance = new YmkInstance(this);
+    const initializer = this.findMethod("init");
+    if (initializer !== null) {
+      initializer.bind(instance).call(interpreter, _arguments);
+    }
+
+    return instance;
+  }
+
+  arity(): number {
+    const initializer = this.findMethod("init");
+    if (initializer === null) return 0;
+    return initializer.arity();
+  }
+
+}
+
+export class YmkFunction implements YmkCallable {
+
+  constructor(
+    private declaration: FuncStmt,
+    private closure: Environment,
+    private isInitializer: boolean) {}
+  
+  bind(instance: YmkInstance): YmkFunction {
+    const environment = new Environment(this.closure);
+    environment.define("this", instance);
+
+    return new YmkFunction(this.declaration,
+      environment, this.isInitializer);
+  }
+
+  toString(): string {
+    return `<fn ${this.declaration.name.lexeme}>`
+  }
+
+  arity(): number { return this.declaration.params.length }
+
+  call(interpreter: Interpreter, _arguments: Object[]): Object | undefined | null {
+    const environment = new Environment(this.closure);
+    const normalParamCount = this.declaration.params.length;
+    // standard args
+    // Normal arguments
+    for (let i = 0; i < normalParamCount && i < _arguments.length; i++) {
+      environment.define(this.declaration.params[i].lexeme, _arguments[i]);
+    }
+    if (this.declaration.hasVarArgs && this.declaration.varArgsName === null) {
+      throw new RuntimeError(null,
+        "Must have var-args name.");
+    }
+    if (this.declaration.hasVarKwargs && this.declaration.kwArgsName === null) {
+      throw new RuntimeError(null,
+        "Must have kwargs name.");
+    }
+
+    // *args
+    if (this.declaration.hasVarArgs) {
+      const varArgs = new Array<Object>();
+      for (let i = normalParamCount; i < _arguments.length; i++) {
+        varArgs.push(_arguments[i]);
+      }
+      environment.define(this.declaration.varArgsName!.lexeme, varArgs);
+    }
+
+    // **kwargs (last arg is expected to be Map<String, Object>)
+    if (this.declaration.hasVarKwargs) {
+      const lastArg = _arguments[_arguments.length - 1];
+      if (!(lastArg instanceof Map)) {
+        throw new RuntimeError(this.declaration.varArgsName,
+          "**kwargs must be passed as Map.");
+      }
+      environment.define(this.declaration.kwArgsName!.lexeme, lastArg);
+    }
+
+    try {
+      interpreter.executeBlock(this.declaration.body, environment);
+    // catch-return 
+    } catch (error) {
+      if (error instanceof Return) {
+        const returnValue: Return = error as Return;
+        // Classes early-return-this
+        if (this.isInitializer) return this.closure.getAt(0, "this");
+
+        return returnValue.value;
       }
 
-      if (this.superclass != null) {
-        return this.superclass.findMethod(name);
-      }
+      // Classes return-this
+      if (this.isInitializer) return this.closure.getAt(0, "this");
 
       return null;
     }
-
-    toString(): string { return this.name; }
-
-    public call(interpreter: Interpreter,
-      _arguments: Object[]
-    ) {
-      const instance = new YmkInstance(this);
-      const initializer = this.findMethod("init");
-      if (initializer !== null) {
-        initializer.bind(instance).call(interpreter, _arguments);
-      }
-
-      return instance;
-    }
-
-    arity(): number {
-      const initializer = this.findMethod("init");
-      if (initializer === null) return 0;
-      return initializer.arity();
-    }
-
   }
 
-  export class YmkFunction implements YmkCallable {
+}
 
-    constructor(
-      private declaration: FuncStmt,
-      private closure: Environment,
-      private isInitializer: boolean) {}
-    
-    bind(instance: YmkInstance): YmkFunction {
-      const environment = new Environment(this.closure);
-      environment.define("this", instance);
+export class YmkLambda implements YmkCallable {
+  constructor(public readonly declaration: LambdaExpr,
+    public readonly closure: Environment,
+    public readonly thisContext: Object | null
+  ) { }
 
-      return new YmkFunction(this.declaration,
-        environment, this.isInitializer);
-    }
-
-    toString(): string {
-      return `<fn ${this.declaration.name.lexeme}>`
-    }
-
-    arity(): number { return this.declaration.params.length }
-
-    call(interpreter: Interpreter, _arguments: Object[]): Object | undefined | null {
-      const environment = new Environment(this.closure);
-      const normalParamCount = this.declaration.params.length;
-      // standard args
-      // Normal arguments
-      for (let i = 0; i < normalParamCount && i < _arguments.length; i++) {
-        environment.define(this.declaration.params[i].lexeme, _arguments[i]);
-      }
-      if (this.declaration.hasVarArgs && this.declaration.varArgsName === null) {
-        throw new RuntimeError(null,
-          "Must have var-args name.");
-      }
-      if (this.declaration.hasVarKwargs && this.declaration.kwArgsName === null) {
-        throw new RuntimeError(null,
-          "Must have kwargs name.");
-      }
-
-      // *args
-      if (this.declaration.hasVarArgs) {
-        const varArgs = new Array<Object>();
-        for (let i = normalParamCount; i < _arguments.length; i++) {
-          varArgs.push(_arguments[i]);
-        }
-        environment.define(this.declaration.varArgsName!.lexeme, varArgs);
-      }
-
-      // **kwargs (last arg is expected to be Map<String, Object>)
-      if (this.declaration.hasVarKwargs) {
-        const lastArg = _arguments[_arguments.length - 1];
-        if (!(lastArg instanceof Map)) {
-          throw new RuntimeError(this.declaration.varArgsName,
-            "**kwargs must be passed as Map.");
-        }
-        environment.define(this.declaration.kwArgsName!.lexeme, lastArg);
-      }
-
-      try {
-        interpreter.executeBlock(this.declaration.body, environment);
-      // catch-return 
-      } catch (error) {
-        if (error instanceof Return) {
-          const returnValue: Return = error as Return;
-          // Classes early-return-this
-          if (this.isInitializer) return this.closure.getAt(0, "this");
-
-          return returnValue.value;
-        }
-
-        // Classes return-this
-        if (this.isInitializer) return this.closure.getAt(0, "this");
-
-        return null;
-      }
-    }
-
+  arity(): number { return this.declaration.params.length }
+  bind(newThis: Object): YmkLambda {
+    return new YmkLambda(this.declaration, this.closure, newThis);
   }
 
-  export class YmkLambda implements YmkCallable {
-    constructor(public readonly declaration: LambdaExpr,
-      public readonly closure: Environment,
-      public readonly thisContext: Object | null
-    ) { }
+  call(interpreter: Interpreter, _arguments: Object[]) {
+    const environment = new Environment(this.closure);
 
-    arity(): number { return this.declaration.params.length }
-    bind(newThis: Object): YmkLambda {
-      return new YmkLambda(this.declaration, this.closure, newThis);
+    for (let i = 0; i < this.declaration.params.length; i++) {
+      environment.define(this.declaration.params[i].lexeme, _arguments[i]);
     }
-
-    call(interpreter: Interpreter, _arguments: Object[]) {
-      const environment = new Environment(this.closure);
-
-      for (let i = 0; i < this.declaration.params.length; i++) {
-        environment.define(this.declaration.params[i].lexeme, _arguments[i]);
-      }
-      if (this.thisContext != null) {
-        environment.define("this", this.thisContext);
-      }
-      return interpreter.evaluateExpr(this.declaration.body, environment);
+    if (this.thisContext != null) {
+      environment.define("this", this.thisContext);
     }
-
-    toString(): string { return '<lambda fn>'; }
+    return interpreter.evaluateExpr(this.declaration.body, environment);
   }
+
+  toString(): string { return '<lambda fn>'; }
+}
 
 
 export class YmkEnv implements YmkCallable {
