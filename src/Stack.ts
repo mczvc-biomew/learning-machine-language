2fee96db2e83b2c922942e34bcef23dbe3deaef1
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-23 15:45:17 +0800

    refactor 0.0.8

--- src/Stack.ts
+++ src/Stack.ts
@@ -1,30 +1,28 @@
-// export namespace io.github.yumika {
-  export class Stack<T> {
+export class Stack<T> {
 
-    private elements: T[] = [];
+  private elements: T[] = [];
 
-    push(element: T): void {
-      this.elements.push(element);
-    }
-
-    pop(): T | undefined {
-      return this.elements.pop();
-    }
-
-    peek(): T | undefined {
-      return this.elements[this.elements.length - 1];
-    }
-
-    get(index: number): T | undefined {
-      return this.elements[index];
-    }
-
-    isEmpty(): boolean {
-      return this.elements.length === 0;
-    }
-
-    size(): number {
-      return this.elements.length;
-    }
+  push(element: T): void {
+    this.elements.push(element);
   }
-// }
+
+  pop(): T | undefined {
+    return this.elements.pop();
+  }
+
+  peek(): T | undefined {
+    return this.elements[this.elements.length - 1];
+  }
+
+  get(index: number): T | undefined {
+    return this.elements[index];
+  }
+
+  isEmpty(): boolean {
+    return this.elements.length === 0;
+  }
+
+  size(): number {
+    return this.elements.length;
+  }
+}
